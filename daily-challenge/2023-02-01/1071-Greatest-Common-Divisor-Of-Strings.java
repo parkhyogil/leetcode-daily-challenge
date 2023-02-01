f85bf0d7131c8c8f5336031a18e4c9dfd3a579ca
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2023-02-01 23:14:20 +0900

    Create 1071-Greatest-Common-Divisor-Of-Strings.java

---
+++ daily-challenge/2023-02-01/1071-Greatest-Common-Divisor-Of-Strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        
+        int gcd = getGCD(str1.length(), str2.length());
+        return str1.substring(0, gcd);
+    }
+
+    private int getGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return getGCD(b, a % b);
+    }
+}
