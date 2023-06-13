7f0f511097a874456cea6329950e6db0665ba6a7
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2023-06-14 02:21:06 +0900

    Update Solution.java

--- problems/2352-equal-row-and-column-pairs/Solution.java
+++ problems/2352-equal-row-and-column-pairs/Solution.java
@@ -1,28 +1,60 @@
 class Solution {
-    private int n;
-    private int[][] grid;
-
     public int equalPairs(int[][] grid) {
-        n = grid.length;
-        this.grid = grid;
+        int n = grid.length;
+
+        Trie trie = new Trie();
+        for (int i = 0; i < n; i++) {
+            trie.insert(grid[i]);
+        }
 
         int res = 0;
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                if (isSameRowAndColumn(r, c)) {
-                    res++;
-                }
+        int[] col = new int[n];
+        for (int c = 0; c < n; c++) {
+            for (int r = 0; r < n; r++) {
+                col[r] = grid[r][c];
             }
+            res += trie.search(col);
         }
         return res;
     }
+}
 
-    private boolean isSameRowAndColumn(int r, int c) {
-        for (int i = 0; i < n; i++) {
-            if (grid[r][i] != grid[i][c]) {
-                return false;
+class Trie {
+    private Node root;
+
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(int[] arr) {
+        Node node = root;
+        for (int num : arr) {
+            if (!node.children.containsKey(num)) {
+                node.children.put(num, new Node());
+            }
+            node = node.children.get(num);
+        }
+        node.count++;
+    }
+
+    public int search(int[] arr) {
+        Node node = root;
+        for (int num : arr) {
+            node = node.children.get(num);
+            if (node == null) {
+                return 0;
             }
         }
-        return true;
+        return node.count;
+    }
+
+    private class Node {
+        private int count;
+        private Map<Integer, Node> children;
+
+        private Node() {
+            count = 0;
+            children = new HashMap<>();
+        }
     }
 }
