53fb939b2a0ec7a16143d34b808b4b354d5c215b
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2023-07-08 17:04:00 +0900

    Create Solution.java

---
+++ problems/2551-put-marbles-in-bags/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long putMarbles(int[] weights, int k) {
+        int n = weights.length;
+
+        if (n == k) {
+            return 0;
+        }
+
+        int[] pairs = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            pairs[i] = weights[i] + weights[i + 1];
+        }
+
+        Arrays.sort(pairs);
+
+        long res = 0;
+        for (int i = 0; i < k - 1; i++) {
+            res += pairs[n - 2 - i] - pairs[i];
+        }
+        return res;
+    }
+}
