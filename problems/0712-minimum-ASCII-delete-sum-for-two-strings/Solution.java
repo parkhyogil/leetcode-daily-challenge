d6632e94fdbf07114e1ada95e1d8a0e452f145b6
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2023-07-31 14:16:56 +0900

    Create Solution.java

---
+++ problems/0712-minimum-ASCII-delete-sum-for-two-strings/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            dp[i + 1][0] = dp[i][0] + s1.charAt(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[0][i + 1] = dp[0][i] + s2.charAt(i);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                } else {
+                    dp[i + 1][j + 1] = Math.min(dp[i + 1][j] + s2.charAt(j), dp[i][j + 1] + s1.charAt(i));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
