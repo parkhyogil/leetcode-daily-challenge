e1c64e5806ac9df054f57fb4bdf4c9f45f017453
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2023-09-24 02:26:14 +0900

    Update Solution.java

--- problems/1048-longest-string-chain/Solution.java
+++ problems/1048-longest-string-chain/Solution.java
@@ -1,40 +1,54 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        Map<String, Integer> chainCount = new HashMap<>();
-        Map<Integer, List<String>> wordsByLength = new HashMap<>();
-        for (int l = 1; l <= 16; l++) {
-            wordsByLength.put(l, new ArrayList<>());
+        int n = words.length;
+
+        List<Integer>[] wordsByLength = new ArrayList[17];
+        for (int i = 0; i <= 16; i++) {
+            wordsByLength[i] = new ArrayList<>();
         }
-        for (String word : words) {
-            chainCount.put(word, 1);
-            wordsByLength.get(word.length()).add(word);
+
+        for (int i = 0; i < n; i++) {
+            wordsByLength[words[i].length()].add(i);
         }
-        
-        int res = 1;
-        for (int l = 1; l <= 15; l++) {
-            for (String curr : wordsByLength.get(l)) {
-                for (String next : wordsByLength.get(l + 1)) {
-                    if (isStrChain(curr, next)) {
-                        chainCount.put(next, Math.max(chainCount.get(next), chainCount.get(curr) + 1));
-                        res = Math.max(res, chainCount.get(next));
+
+        int[] dp = new int[n];
+        int res = 0;
+        for (int len = 1; len <= 16; len++) {
+            for (int i : wordsByLength[len]) {
+                dp[i] = 1;
+
+                for (int j : wordsByLength[len - 1]) {
+                    if (isPredecessor(words[j], words[i])) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
                     }
                 }
+
+                res = Math.max(res, dp[i]);
             }
         }
         return res;
     }
-    
-    public boolean isStrChain(String curr, String next) {
+
+    private boolean isPredecessor(String a, String b) {
+        int lenA = a.length();
+        int lenB = b.length();
+
+        if (lenB - lenA != 1) {
+            return false;
+        }
+
         int i = 0;
         int j = 0;
-        while (i < curr.length() && j < next.length()) {
-            if (curr.charAt(i) == next.charAt(j)) {
+
+        while (i < lenA && j < lenB) {
+            if (a.charAt(i) == b.charAt(j)) {
                 i++;
                 j++;
             } else {
                 j++;
             }
         }
-        return i == curr.length();
+
+        return j - i <= 1;
     }
 }
