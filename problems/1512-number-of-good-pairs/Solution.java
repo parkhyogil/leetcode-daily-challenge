866207990a085df4a13a7ff4126f6f33f6de665a
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2023-10-03 14:06:10 +0900

    Create Solution.java

---
+++ problems/1512-number-of-good-pairs/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int[] count = new int[101];
+
+        int res = 0;
+        for (int num : nums) {
+            res += count[num];
+            count[num]++;
+        }
+        return res;
+    }
+}
