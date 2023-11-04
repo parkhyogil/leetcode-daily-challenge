0680290e51d44e0a2a90d48a66d0b785671380b1
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2023-11-04 21:46:24 +0900

    Create Solution.java

---
+++ problems/1503-last-moment-before-all-ants-fall-out-of-a-plank/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int res = 0;
+
+        for (int a : left) {
+            res = Math.max(res, n);
+        }
+
+        for (int a : right) {
+            res = Math.max(res, n - a);
+        }
+
+        return res;
+    }
+}
