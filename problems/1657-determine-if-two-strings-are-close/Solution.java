752b305b91cda42102cbc71edc95a19757ea22e0
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2024-01-15 00:02:27 +0900

    Create Solution.java

---
+++ problems/1657-determine-if-two-strings-are-close/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        int n = word1.length();
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            count1[word1.charAt(i) - 'a']++;
+            count2[word2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] > 0 ^ count2[i] > 0) {
+                return false;
+            }
+        }
+
+        Arrays.sort(count1);
+        Arrays.sort(count2);
+
+        for (int i = 0; i < 26; i++) {
+            if (count1[i] != count2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
