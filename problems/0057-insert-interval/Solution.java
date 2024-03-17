160e1a781182d1cad58fb356a88365cfbf3ee9eb
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2024-03-17 19:21:23 +0900

    Update Solution.java

--- problems/0057-insert-interval/Solution.java
+++ problems/0057-insert-interval/Solution.java
@@ -2,28 +2,32 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
 
-        List<int[]> res = new ArrayList<>();
+        int i = 0;
+        List<int[]> list = new ArrayList<>();
 
-        int idx = 0;
-        while (idx < n && newInterval[1] >= intervals[idx][0]) {
-            if (intervals[idx][1] < newInterval[0]) {
-                res.add(intervals[idx++]);
-            } else {
-                merge(newInterval, intervals[idx++]);
-            }
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            list.add(intervals[i++]);
         }
 
-        res.add(newInterval);
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+
+        list.add(newInterval);        
+
+        while (i < n) {
+            list.add(intervals[i++]);
+        }
+
+        int[][] res = new int[list.size()][];
+        i = 0;
         
-        while (idx < n) {
-            res.add(intervals[idx++]);
+        while (i < list.size()) {
+            res[i] = list.get(i++);
         }
-
-        return res.toArray(new int[res.size()][]);
-    }
-
-    private void merge(int[] newInterval, int[] interval) {
-        newInterval[0] = Math.min(newInterval[0], interval[0]);
-        newInterval[1] = Math.max(newInterval[1], interval[1]);
+        
+        return res;
     }
 }
