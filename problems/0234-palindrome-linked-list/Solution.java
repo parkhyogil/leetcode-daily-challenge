6db4ac5b26e641f11139c9f0f613d35c21ee8c3d
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2024-03-22 23:11:52 +0900

    Create Solution.java

---
+++ problems/0234-palindrome-linked-list/Solution.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode tail = reverseLinkedList(getMiddleNode(head));
+        
+        while (head != null && tail != null) {
+            if (head.val != tail.val) {
+                return false;
+            }
+            head = head.next;
+            tail = tail.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+
+    private ListNode getMiddleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
