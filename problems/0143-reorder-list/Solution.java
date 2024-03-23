6d2761d311d3bbf28042507b63a6376824ec6a40
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2024-03-23 20:28:47 +0900

    Create Solution.java

---
+++ problems/0143-reorder-list/Solution.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode tail = reverseList(splitList(head));
+
+        while (head != null && tail != null) {
+            ListNode nextTail = tail.next;
+
+            tail.next = head.next;
+            head.next = tail;
+
+            head = head.next.next;
+            tail = nextTail;
+        }
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+
+    private ListNode splitList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode res = slow.next;
+        slow.next = null;
+
+        return res;
+    }
+}
