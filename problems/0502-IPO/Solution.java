9794165a65b1e7a42259d1f5e54caa3b31ebf898
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2024-06-15 16:24:03 +0900

    Update Solution.java

--- problems/0502-IPO/Solution.java
+++ problems/0502-IPO/Solution.java
@@ -2,26 +2,25 @@ class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
         int n = profits.length;
 
-        int[][] projects = new int[n][2];
+        PriorityQueue<int[]> sortedProjects = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        PriorityQueue<Integer> sortedProfits = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+
         for (int i = 0; i < n; i++) {
-            projects[i][0] = profits[i];
-            projects[i][1] = capital[i];
-        }
-        
-        Arrays.sort(projects, (a, b) -> Integer.compare(a[1], b[1]));
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
-
-        int idx = 0;
-        while (idx < n && w >= projects[idx][1]) {
-            pq.offer(projects[idx++][0]);
-        }
-        while (!pq.isEmpty() && k-- > 0) {
-            w += pq.poll();
-            while (idx < n && w >= projects[idx][1]) {
-                pq.offer(projects[idx++][0]);
+            if (capital[i] <= w) {
+                sortedProfits.offer(profits[i]);
+            } else {
+                sortedProjects.offer(new int[] {profits[i], capital[i]});
             }
         }
+
+        for (int i = 0; i < k && !sortedProfits.isEmpty(); i++) {
+            w += sortedProfits.poll();
+
+            while (!sortedProjects.isEmpty() && sortedProjects.peek()[1] <= w) {
+                sortedProfits.offer(sortedProjects.poll()[0]);
+            }
+        }
+
         return w;
     }
 }
