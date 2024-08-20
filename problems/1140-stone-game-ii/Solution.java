7cc1c79fe1193d361345bf26d1b7c2fabd4b7ded
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2024-08-20 16:16:52 +0900

    Update Solution.java

--- problems/1140-stone-game-ii/Solution.java
+++ problems/1140-stone-game-ii/Solution.java
@@ -1,31 +1,40 @@
 class Solution {
     public int stoneGameII(int[] piles) {
         int n = piles.length;
-        Integer[][] memo = new Integer[n][n + 1];
-        int diff = recur(0, 1, piles, memo);
-        
-        int sum = 0;
-        for (int i : piles) {
-            sum += i;
+
+        int[][] cache = new int[n][n];
+        for (int[] arr : cache) {
+            Arrays.fill(arr, -1);
         }
+
+        int sum = 0;
+        for (int pile : piles) {
+            sum += pile;
+        }
+
+        int diff = recur(0, 1, piles, cache);
+
         return (sum + diff) / 2;
     }
-    
-    public int recur(int i, int m, int[] nums, Integer[][] memo) {
-        if (i >= memo.length) {
+
+    private int recur(int idx, int m, int[] piles, int[][] cache) {
+        if (idx == piles.length) {
             return 0;
         }
-        if (memo[i][m] == null) {
-            int res = Integer.MIN_VALUE;
-            int sum = 0;
-            for (int x = 0; x < m * 2; x++) {
-                if (i + x < nums.length) {
-                    sum += nums[i + x];
-                    res = Math.max(res, sum - recur(i + x + 1, Math.max(m, x + 1), nums, memo));
-                }
-            }
-            memo[i][m] = res;
+
+        if (cache[idx][m] != -1) {
+            return cache[idx][m];
         }
-        return memo[i][m];
+
+        int sum = 0;
+        int result = Integer.MIN_VALUE;
+
+        for (int x = 1; x <= 2 * m && idx + x <= piles.length; x++) {
+            sum += piles[idx + x - 1];
+
+            result = Math.max(result, sum - recur(idx + x, Math.max(x, m), piles, cache));
+        }
+
+        return cache[idx][m] = result;
     }
 }
