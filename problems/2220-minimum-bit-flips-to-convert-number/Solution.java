699e7a6cad7ee3d482ad292a47e82829cff5fc1e
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2024-09-11 15:45:14 +0900

    Create Solution.java

---
+++ problems/2220-minimum-bit-flips-to-convert-number/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        return countBit(start ^ goal);
+    }
+
+    private int countBit(int num) {
+        num = (num & 0x55555555) + ((num >> 1) & 0x55555555);
+        num = (num & 0x33333333) + ((num >> 2) & 0x33333333);
+        num = (num & 0x0F0F0F0F) + ((num >> 4) & 0x0F0F0F0F);
+        num = (num & 0x00FF00FF) + ((num >> 8) & 0x00FF00FF);
+        num = (num & 0x0000FFFF) + ((num >> 16) & 0x0000FFFF);
+
+        return num;
+    }
+}
