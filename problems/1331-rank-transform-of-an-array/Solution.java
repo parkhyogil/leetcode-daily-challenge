f8fb2afcb0d7f210b7000942f33ebd239092bb64
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2024-10-02 13:47:20 +0900

    Create Solution.java

---
+++ problems/1331-rank-transform-of-an-array/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int n = arr.length;
+
+        if (n == 0) {
+            return new int[0];
+        }
+
+        Integer[] indexArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indexArray[i] = i;
+        }
+
+        Arrays.sort(indexArray, (a, b) -> Integer.compare(arr[a], arr[b]));
+
+        int[] rankArray = new int[n];
+        rankArray[indexArray[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[indexArray[i]] > arr[indexArray[i - 1]]) {
+                rankArray[indexArray[i]] = rankArray[indexArray[i - 1]] + 1;
+            } else {
+                rankArray[indexArray[i]] = rankArray[indexArray[i - 1]];
+            }
+        }
+
+        return rankArray;
+    }
+}
