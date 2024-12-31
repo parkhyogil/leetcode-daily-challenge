4f0dfad9ba11776afb1a8ad4431ae1702d405a86
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2024-12-31 23:39:05 +0900

    Update Solution.java

--- problems/0983-minimum-cost-for-tickets/Solution.java
+++ problems/0983-minimum-cost-for-tickets/Solution.java
@@ -3,20 +3,27 @@ class Solution {
         int n = days.length;
 
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
 
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n && days[j] < days[i] + 1; j++) {
-                dp[j + 1] = Math.min(dp[j + 1], dp[i] + costs[0]);
-            }
-            for (int j = i; j < n && days[j] < days[i] + 7; j++) {
-                dp[j + 1] = Math.min(dp[j + 1], dp[i] + costs[1]);
-            }
-            for (int j = i; j < n && days[j] < days[i] + 30; j++) {
-                dp[j + 1] = Math.min(dp[j + 1], dp[i] + costs[2]);
+            dp[i + 1] = Integer.MAX_VALUE;
+
+            for (int j = i; j >= 0 && days[i] - days[j] < 30; j--) {
+                int diff = days[i] - days[j];
+
+                if (diff < 1) {
+                    dp[i + 1] = Math.min(dp[i + 1], dp[j] + costs[0]);
+                }
+
+                if (diff < 7) {
+                    dp[i + 1] = Math.min(dp[i + 1], dp[j] + costs[1]);
+                }
+
+                if (diff < 30) {
+                    dp[i + 1] = Math.min(dp[i + 1], dp[j] + costs[2]);
+                }
             }
         }
+
         return dp[n];
     }
 }
