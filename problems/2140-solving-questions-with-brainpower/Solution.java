76df88847b2ab457545d90a81c9dc39ed7046a65
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2025-04-01 21:45:17 +0900

    Update Solution.java

--- problems/2140-solving-questions-with-brainpower/Solution.java
+++ problems/2140-solving-questions-with-brainpower/Solution.java
@@ -2,15 +2,19 @@ class Solution {
     public long mostPoints(int[][] questions) {
         int n = questions.length;
 
-        long[] dp = new long[n + 1];
+        long[] dp = new long[n];
+        dp[n - 1] = questions[n - 1][0];
 
-        for (int i = n - 1; i >= 0; i--) {
-            int point = questions[i][0];
-            int brainpower = questions[i][1];
-            int next = i + brainpower + 1;
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i] = questions[i][0];
 
-            dp[i] = Math.max(dp[i + 1], point + (next < n ? dp[next] : 0));
+            if (i + questions[i][1] + 1 < n) {
+                dp[i] += dp[i + questions[i][1] + 1];
+            }
+
+            dp[i] = Math.max(dp[i], dp[i + 1]);
         }
+
         return dp[0];
     }
 }
