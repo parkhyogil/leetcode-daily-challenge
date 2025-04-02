41004f6b17b9276d9dd3e77f139d400a7cba158a
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2025-04-02 23:15:46 +0900

    Create Solution.java

---
+++ problems/2837-maximum-value-of-an-ordered-triplet-i/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        int n = nums.length;
+
+        long result = 0L;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    result = Math.max(result, (long) (nums[i] - nums[j]) * nums[k]);
+                }
+            }
+        }
+
+        return result;
+    }
+}
