876e38c57021c4300628afa96d3e44afaca05c21
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2025-04-06 21:41:08 +0900

    Update Solution.java

--- problems/0368-largest-divisible-subset/Solution.java
+++ problems/0368-largest-divisible-subset/Solution.java
@@ -4,25 +4,34 @@ class Solution {
 
         Arrays.sort(nums);
 
-        List<Integer>[] arr = new ArrayList[n];
-        List<Integer> res = new ArrayList<>();
+        int[] dp = new int[n];
+        int[] prev = new int[n];
+
+        int maxIdx = 0;
 
         for (int i = 0; i < n; i++) {
-            arr[i] = new ArrayList<>();
+            dp[i] = 1;
+            prev[i] = -1;
 
             for (int j = i - 1; j >= 0; j--) {
-                if (nums[i] % nums[j] == 0 && arr[j].size() > arr[i].size()) {
-                    arr[i] = new ArrayList<>(arr[j]);
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
                 }
             }
 
-            arr[i].add(nums[i]);
-
-            if (arr[i].size() > res.size()) {
-                res = arr[i];
+            if (dp[maxIdx] < dp[i]) {
+                maxIdx = i;
             }
         }
 
-        return res;
+        List<Integer> result = new ArrayList<>();
+        
+        while (maxIdx != -1) {
+            result.add(nums[maxIdx]);
+            maxIdx = prev[maxIdx];
+        }
+
+        return result;
     }
 }
