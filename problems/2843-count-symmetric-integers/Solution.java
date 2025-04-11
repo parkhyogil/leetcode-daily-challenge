ce2df96014f3ebe263638bf5bc96b2e1c5e64e24
Hyogil <53911346+parkhyogil@users.noreply.github.com>
2025-04-11 23:08:23 +0900

    Create Solution.java

---
+++ problems/2843-count-symmetric-integers/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int result = 0;
+
+        for (int i = low; i <= high; i++) {
+            if (i < 100) {
+                result += i % 11 == 0 ? 1 : 0;
+            } else if (i > 1000) {
+                int left = i / 1000 + i % 1000 / 100;
+                int right = i % 100 / 10 + i % 10;
+
+                if (left == right) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
