18b48bc7805b6e0f249b2e18f0d6687dcd724880
hyogil <hg19910@gmail.com>
2025-05-11 11:07:43 +0900

    solve 1550-three-consecutive-odds

--- problems/1550-three-consecutive-odds/Solution.java
+++ problems/1550-three-consecutive-odds/Solution.java
@@ -2,14 +2,12 @@ class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         int n = arr.length;
 
-        for (int l = 0, r = 0; r < n; r++) {
-            if (arr[r] % 2 == 0) {
-                l = r + 1;
-            } else if (r - l + 1 == 3) {
+        for (int i = 2; i < n; i++) {
+            if (arr[i] % 2 == 1 && arr[i - 1] % 2 == 1 && arr[i - 2] % 2 == 1) {
                 return true;
             }
         }
-      
+
         return false;
     }
 }
