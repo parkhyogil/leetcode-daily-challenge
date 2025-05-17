4deac90e8b21a58ad6d657d1daff96621100af67
hyogil <hg19910@gmail.com>
2025-05-17 11:55:17 +0900

    solve 0075-sort-colors

--- problems/0075-sort-colors/Solution.java
+++ problems/0075-sort-colors/Solution.java
@@ -1,26 +1,21 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
+        
+        int l = -1, r = n;
 
-        int zero = -1;
-        int two = n;
-
-        for (int i = 0; i < two; ) {
-            int num = nums[i];
-
-            if (num == 0) {
-                swap(++zero, i++, nums);
-            } else if (num == 1) {
-                i++;
-            } else {
-                swap(--two, i, nums);
+        for (int i = 0; i < r; i++) {
+            if (nums[i] == 0) {
+                swap(++l, i, nums);
+            } else if (nums[i] == 2) {
+                swap(--r, i--, nums);
             }
         }
     }
 
-    private void swap(int i, int j, int[] nums) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    private void swap(int i, int j, int[] arr) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
