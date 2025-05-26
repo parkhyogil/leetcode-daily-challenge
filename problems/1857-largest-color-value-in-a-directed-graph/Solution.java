1c8d5458e3f231353f576176675d435ed0e6b799
hyogil <hg19910@gmail.com>
2025-05-26 15:16:31 +0900

    solve 1857-largest-color-value-in-a-directed-graph

--- problems/1857-largest-color-value-in-a-directed-graph/Solution.java
+++ problems/1857-largest-color-value-in-a-directed-graph/Solution.java
@@ -2,45 +2,65 @@ class Solution {
     public int largestPathValue(String colors, int[][] edges) {
         int n = colors.length();
 
+        List<List<Integer>> graph = buildGraph(n, edges);
+        int[][] count = new int[n][26];
+
+        boolean[] visited = new boolean[n];
+        boolean[] checking = new boolean[n];
+
+        int maxColor = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (dfs(i, colors, graph, count, visited, checking)) {
+                return -1;
+            }
+
+            maxColor = Math.max(maxColor, count[i][colors.charAt(i) - 'a']);
+        }
+
+        return maxColor;
+    }
+
+    private boolean dfs(int node, String colors, List<List<Integer>> graph, int[][] count, boolean[] visited, boolean[] checking) {
+        if (checking[node]) {
+            return true;
+        }
+
+        if (visited[node]) {
+            return false;
+        }
+        
+        visited[node] = true;
+        checking[node] = true;
+
+        for (int next : graph.get(node)) {
+            if (dfs(next, colors, graph, count, visited, checking)) {
+                return true;
+            }
+
+            for (int i = 0; i < 26; i++) {
+                count[node][i] = Math.max(count[node][i], count[next][i]);
+            }
+        }
+
+        count[node][colors.charAt(node) - 'a']++;
+        
+        checking[node] = false;
+
+        return false;
+    }
+
+    private List<List<Integer>> buildGraph(int n, int[][] edges) {
         List<List<Integer>> graph = new ArrayList<>();
+        
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        int[] indegree = new int[n];
         for (int[] e : edges) {
             graph.get(e[0]).add(e[1]);
-            indegree[e[1]]++;
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
-            }
-        }
-
-        int res = 0;
-        int passedNode = 0;
-        int[][] count = new int[n][26];
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-            int color = colors.charAt(node) - 'a';
-            res = Math.max(res, ++count[node][color]);
-            passedNode++;
-
-            for (int next : graph.get(node)) {
-                indegree[next]--;
-                for (int i = 0; i < 26; i++) {
-                    count[next][i] = Math.max(count[next][i], count[node][i]);
-                }
-                if (indegree[next] == 0) {
-                    q.offer(next);
-                }
-            }
-        }
-
-        return passedNode == n ? res : -1;
+        
+        return graph;
     }
 }
