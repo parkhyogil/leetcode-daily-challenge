f1489c5856be1cfa59ef4d3645bbd44788a6986a
hyogil <hg19910@gmail.com>
2025-06-05 23:26:44 +0900

    solve 1061-lexicographically-smallest-equivalent-string

--- problems/1061-lexicographically-smallest-equivalent-string/Solution.java
+++ problems/1061-lexicographically-smallest-equivalent-string/Solution.java
@@ -1,38 +1,41 @@
 class Solution {
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
         int n = s1.length();
+        int m = baseStr.length();
 
-        int[] root = new int[26];
+        int[] roots = new int[26];
         for (int i = 0; i < 26; i++) {
-            root[i] = i;
+            roots[i] = i;
         }
 
         for (int i = 0; i < n; i++) {
-            union(s1.charAt(i) - 'a', s2.charAt(i) - 'a', root);
+            union(s1.charAt(i) - 'a', s2.charAt(i) - 'a', roots);
         }
 
-        StringBuilder sb = new StringBuilder(n);
-        for (char c : baseStr.toCharArray()) {
-            sb.append((char) ('a' + getRoot(c - 'a', root)));
+        char[] chars = new char[m];
+        
+        for (int i = 0; i < m; i++) {
+            chars[i] = (char) (findRoot(baseStr.charAt(i) - 'a', roots) + 'a');
         }
-        return sb.toString();
+
+        return String.valueOf(chars);
     }
 
-    private void union(int c1, int c2, int[] root) {
-        c1 = getRoot(c1, root);
-        c2 = getRoot(c2, root);
+    private void union(int a, int b, int[] roots) {
+        a = findRoot(a, roots);
+        b = findRoot(b, roots);
 
-        if (c1 < c2) {
-            root[c2] = c1;
+        if (a < b) {
+            roots[b] = a;
         } else {
-            root[c1] = c2;
+            roots[a] = b;
         }
     }
 
-    private int getRoot(int child, int[] root) {
-        if (child == root[child]) {
+    private int findRoot(int child, int[] roots) {
+        if (child == roots[child]) {
             return child;
         }
-        return root[child] = getRoot(root[child], root);
+        return roots[child] = findRoot(roots[child], roots);
     }
 }
