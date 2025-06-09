6e2d2ec847f85d7381579bdb0029c7eca030f6de
hyogil <hg19910@gmail.com>
2025-06-09 22:34:38 +0900

    solve 0440-k-th-smallest-in-lexicographically-order

--- problems/0440-k-th-smallest-in-lexicographical-order/Solution.java
+++ problems/0440-k-th-smallest-in-lexicographical-order/Solution.java
@@ -1,50 +1,48 @@
 class Solution {
     public int findKthNumber(int n, int k) {
-        for (int i = 1; i <= 9; i++) {
-            int subTreeCount = countSubTree(i, n);
+        for (int i = 1; i < 10; i++) {
+            int count = countSubtree(i, n);
 
-            if (subTreeCount >= k) {
-                return findInSubTree(i, n, k);
+            if (count >= k) {
+                return find(i, n, k);
             } else {
-                k -= subTreeCount;
+                k -= count;
             }
         }
 
         return -1;
     }
 
-    int findInSubTree(int prefix, int n, int k) {
+    private int find(int prefix, int n, int k) {
         if (k == 1) {
             return prefix;
         }
-
         k--;
 
-        for (int i = 0; i <= 9; i++) {
-            int nextPrefix = prefix * 10 + i;
-            int subTreeCount = countSubTree(nextPrefix, n);
+        for (int i = 0; i < 10; i++) {
+            int count = countSubtree(prefix * 10 + i, n);
 
-            if (subTreeCount >= k) {
-                return findInSubTree(nextPrefix, n, k);
+            if (count >= k) {
+                return find(prefix * 10 + i, n, k);
             } else {
-                k -= subTreeCount;
+                k -= count;
             }
         }
 
         return -1;
     }
 
-    int countSubTree(int i, int n) {
-        int result = 1;
+    private int countSubtree(int num, int n) {
+        long min = num;
+        long max = num;
 
-        long begin = i * 10L;
-        long end = begin + 9;
+        int result = 0;
 
-        while (begin <= n) {
-            result += Math.min(n, end) - begin + 1;
+        while (min <= n) {
+            result += Math.min(n, max) - min + 1;
 
-            begin *= 10;
-            end = end * 10 + 9;
+            min *= 10;
+            max = max * 10 + 9;
         }
 
         return result;
