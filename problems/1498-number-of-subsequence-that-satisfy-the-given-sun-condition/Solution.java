4c9ffa9e3c0fe5f3613bef8882b3dab6ed650ad7
hyogil <hg19910@gmail.com>
2025-06-29 21:21:17 +0900

    solve 1498-number-of-subsequences-that-satisfy-the-given-sum-condition

--- problems/1498-number-of-subsequence-that-satisfy-the-given-sun-condition/Solution.java
+++ problems/1498-number-of-subsequence-that-satisfy-the-given-sun-condition/Solution.java
@@ -4,23 +4,27 @@ class Solution {
     public int numSubseq(int[] nums, int target) {
         int n = nums.length;
 
+        int[] pow = new int[n];
+
+        pow[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pow[i] = pow[i - 1] * 2 % MOD;
+        }
+
         Arrays.sort(nums);
 
-        int[] powers = new int[n];
-        powers[0] = 1;
-        for (int i = 1; i < n; i++) {
-            powers[i] = (powers[i - 1] * 2) % MOD;
-        }
+        int result = 0;
 
-        int res = 0;
-        for (int left = 0, right = n - 1; left <= right; ) {
-            if (nums[left] + nums[right] <= target) {
-                res = (res + powers[right - left]) % MOD;
-                left++;
-            } else {
-                right--;
+        for (int i = 0, j = n - 1; i <= j; i++) {
+            while (i <= j && nums[i] + nums[j] > target) {
+                j--;
+            }
+
+            if (i <= j) {
+                result = (result + pow[j - i]) % MOD;
             }
         }
-        return res;
+
+        return result;
     }
 }
