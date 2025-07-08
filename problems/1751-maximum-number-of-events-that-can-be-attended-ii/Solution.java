daec16ed75548bd051aacfc131dd422d87917815
hyogil <hg19910@gmail.com>
2025-07-08 13:55:43 +0900

    solve 1751-maximum-number-of-events-that-can-be-attended-ii

--- problems/1751-maximum-number-of-events-that-can-be-attended-ii/Solution.java
+++ problems/1751-maximum-number-of-events-that-can-be-attended-ii/Solution.java
@@ -1,32 +1,56 @@
 class Solution {
     public int maxValue(int[][] events, int k) {
-        int n = events.length;
+        if (k == 1) {
+            int max = 0;
 
-        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
+            for (int[] e : events) {
+                max = Math.max(max, e[2]);
+            }
 
-        int[][] memo = new int[n][k + 1];
-        for (int[] m : memo) {
-            Arrays.fill(m, -1);
+            return max;
         }
         
-        return recur(0, -1, events, k, memo);
+        int n = events.length;
+
+        Arrays.sort(events, (a, b) -> a[1] - b[1]);
+
+        int[] prev = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            prev[i] = binarySearch(events[i][0], i - 1, events);
+        }
+
+        return recur(n - 1, k, events, prev, new int[n][k + 1]);
     }
 
-    private int recur(int idx, int endDay, int[][] events, int k, int[][] memo) {
-        if (k == 0 || idx == events.length) {
+    private int recur(int i, int k, int[][] events, int[] prev, int[][] cache) {
+        if (i < 0 || k == 0) {
             return 0;
         }
 
-        if (memo[idx][k] != -1) {
-            return memo[idx][k];
+        if (cache[i][k] != 0) {
+            return cache[i][k];
         }
 
-        int res = 0;
-        for (int i = idx; i < events.length; i++) {
-            if (endDay < events[i][0]) {
-                res = Math.max(res, events[i][2] + recur(i + 1, events[i][1], events, k - 1, memo));
+        int attend = events[i][2] + recur(prev[i], k - 1, events, prev, cache);
+        int notAttend = recur(i - 1, k, events, prev, cache);
+
+        return cache[i][k] = Math.max(attend, notAttend);
+    }
+
+    private int binarySearch(int target, int hi, int[][] events) {
+        int lo = 0;
+
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            if (events[mid][1] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
             }
         }
-        return memo[idx][k] = res;
+
+        return hi;
     }
 }
