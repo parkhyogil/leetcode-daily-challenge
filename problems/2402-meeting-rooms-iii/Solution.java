8db5e19689b4a772cf3ef6e03fb18ae4c5cd2958
hyogil <hg19910@gmail.com>
2025-07-11 14:30:36 +0900

    solve 2402-meeting-rooms-ii

--- problems/2402-meeting-rooms-iii/Solution.java
+++ problems/2402-meeting-rooms-iii/Solution.java
@@ -1,85 +1,48 @@
 class Solution {
-    class Manager {
-        class Book implements Comparable<Book> {
-            private long endTime;
-            private int roomNumber;
-
-            public Book(long endTime, int roomNumber) {
-                this.endTime = endTime;
-                this.roomNumber = roomNumber;
-            }
-
-            @Override
-            public int compareTo(Book o) {
-                if (this.endTime == o.endTime) {
-                    return Integer.compare(this.roomNumber, o.roomNumber);
-                }
-                return Long.compare(this.endTime, o.endTime);
-            }
-        }
-
-        private int size;
-        private int[] bookedCount;
-        private PriorityQueue<Book> bookQueue;
-        private PriorityQueue<Integer> roomQueue;
-
-        public Manager(int size) {
-            this.size = size;
-
-            bookedCount = new int[size];
-            bookQueue = new PriorityQueue<>(size);
-            roomQueue = new PriorityQueue<>();
-            
-            for (int i = 0; i < size; i++) {
-                roomQueue.offer(i);
-            }
-        }
-
-        private long getStartTime(int[] time) {
-            long startTime = time[0];
-
-            while (!bookQueue.isEmpty() && bookQueue.peek().endTime <= startTime) {
-                roomQueue.offer(bookQueue.poll().roomNumber);
-            }
-
-            if (bookQueue.size() == size) {
-                startTime = bookQueue.peek().endTime;
-                roomQueue.offer(bookQueue.poll().roomNumber);
-            }
-
-            return startTime;
-        }
-
-        public void bookRoom(int[] time) {
-            long startTime = getStartTime(time);
-            int roomNumber = roomQueue.poll();
-            
-            bookQueue.offer(new Book(startTime + time[1] - time[0], roomNumber));
-            bookedCount[roomNumber]++;
-        }
-
-        public int getMostBookedRoom() {
-            int res = 0;
-
-            for (int i = 0; i < size; i++) {
-                if (bookedCount[res] < bookedCount[i]) {
-                    res = i;
-                }
-            }
-
-            return res;
-        }
-    }
-
     public int mostBooked(int n, int[][] meetings) {
-        Manager manager = new Manager(n);
+        PriorityQueue<Integer> rooms = new PriorityQueue<>();
+        PriorityQueue<int[]> bookedRooms = new PriorityQueue<>((a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
 
-        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
-
-        for (int[] meeting  : meetings) {
-            manager.bookRoom(meeting);
+        for (int i = 0; i < n; i++) {
+            rooms.offer(i);
         }
 
-        return manager.getMostBookedRoom();
+        int[] numBooked = new int[n];
+
+        Arrays.sort(meetings, (a, b) -> a[0] - b[0]);
+
+        for (int[] meeting : meetings) {
+            int start = meeting[0];
+            int end = meeting[1];
+
+            while (!bookedRooms.isEmpty() && bookedRooms.peek()[1] <= start) {
+                rooms.offer(bookedRooms.poll()[0]);
+            }
+
+            int room;
+            int endTime;
+            if (rooms.isEmpty()) {
+                int[] lastBook = bookedRooms.poll();
+
+                room = lastBook[0];
+                endTime = lastBook[1] + end - start;
+            } else {
+                room = rooms.poll();
+                endTime = end;
+            }
+
+            numBooked[room]++;
+            bookedRooms.offer(new int[] {room, endTime});
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (numBooked[i] > numBooked[result]) {
+                result = i;
+            }
+        }
+
+        return result;
     }
 }
