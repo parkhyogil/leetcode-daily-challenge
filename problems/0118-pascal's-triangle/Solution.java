529971a17892605e070279668462486467c58524
hyogil <hg19910@gmail.com>
2025-08-01 14:01:48 +0900

    solve 0118-pascals-triangle

--- problems/0118-pascal's-triangle/Solution.java
+++ problems/0118-pascal's-triangle/Solution.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
 
-        res.add(Arrays.asList(1));
+        result.add(List.of(1));
 
-        for (int i = 2; i <= numRows; i++) {
+        for (int i = 1; i < numRows; i++) {
             List<Integer> row = new ArrayList<>();
-            List<Integer> prevRow = res.get(i - 2);
 
             row.add(1);
-            for (int j = 0; j < prevRow.size() - 1; j++) {
-                row.add(prevRow.get(j) + prevRow.get(j + 1));
+
+            for (int j = 1; j < i; j++) {
+                row.add(result.get(i - 1).get(j) + result.get(i - 1).get(j - 1));
             }
+
             row.add(1);
-            res.add(row);
+
+            result.add(row);
         }
 
-        return res;
+        return result;
     }
 }
