06a260e8e33e222edd3a31c21efeccd6b3911436
hyogil <hg19910@gmail.com>
2025-08-20 13:55:53 +0900

    solve 1277-count-square-submatrices-with-all-ones

--- problems/1277-count-square-submatrices-with-all-ones/Solution.java
+++ problems/1277-count-square-submatrices-with-all-ones/Solution.java
@@ -1,26 +1,16 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+        int m = matrix.length, n = matrix[0].length;
 
         int result = 0;
 
         for (int i = 0; i < m; i++) {
-            result += matrix[i][0];
-        }
-        for (int i = 1; i < n; i++) {
-            result += matrix[0][i];
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    continue;
+            for (int j = 0; j < n; j++) {
+                if (i > 0 && j > 0 && matrix[i][j] == 1) {
+                    matrix[i][j] = Math.min(matrix[i][j - 1], Math.min(matrix[i - 1][j], matrix[i - 1][j - 1])) + 1;
                 }
-
-                matrix[i][j] += Math.min(matrix[i - 1][j], Math.min(matrix[i][j - 1], matrix[i - 1][j - 1]));
-
-                result += matrix[i][j];                
+                
+                result += matrix[i][j];
             }
         }
 
