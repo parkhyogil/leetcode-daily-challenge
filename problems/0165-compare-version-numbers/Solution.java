9e884c6d18df0fbff12314426e528a35ab85034e
hyogil <hg19910@gmail.com>
2025-09-23 10:23:37 +0900

    solve 0165-compare-version-numbers

--- problems/0165-compare-version-numbers/Solution.java
+++ problems/0165-compare-version-numbers/Solution.java
@@ -1,29 +1,29 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        int m = version1.length();
-        int n = version2.length();
+        int n = version1.length();
+        int m = version2.length();
 
         int i = 0;
         int j = 0;
 
-        while (i < m || j < n) {
+        while (i < n || j < m) {
             int v1 = 0;
-            while (i < m && version1.charAt(i) != '.') {
-                v1 = v1 * 10 + (version1.charAt(i) - '0');
-                i++;
-            }   
+            int v2 = 0;
+
+            while (i < n && version1.charAt(i) != '.') {
+                v1 = v1 * 10 + version1.charAt(i++) - '0';
+            }
             i++;
 
-            int v2 = 0;
-            while (j < n && version2.charAt(j) != '.') {
-                v2 = v2 * 10 + (version2.charAt(j) - '0');
-                j++;
-            }   
+            while (j < m && version2.charAt(j) != '.') {
+                v2 = v2 * 10 + version2.charAt(j++) - '0';
+            }
             j++;
 
             if (v1 < v2) {
                 return -1;
-            } else if (v1 > v2) {
+            }
+            if (v1 > v2) {
                 return 1;
             }
         }
