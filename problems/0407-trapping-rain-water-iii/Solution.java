e01c2cf7737d250fb8f27ea9f4d8397ed00855a8
hyogil <hg19910@gmail.com>
2025-10-03 23:09:33 +0900

    solve 0407-trapping-rain-water-ii

--- problems/0407-trapping-rain-water-iii/Solution.java
+++ problems/0407-trapping-rain-water-iii/Solution.java
@@ -1,59 +1,51 @@
 class Solution {
-    private int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
     public int trapRainWater(int[][] heightMap) {
-        int m = heightMap.length;
-        int n = heightMap[0].length;
+        int m = heightMap.length, n = heightMap[0].length;
+        int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         boolean[][] visited = new boolean[m][n];
 
         for (int i = 0; i < m; i++) {
-            priorityQueue.offer(new int[] {i, 0, heightMap[i][0]});
-            priorityQueue.offer(new int[] {i, n - 1, heightMap[i][n - 1]});
-
+            queue.offer(new int[] {i, 0, heightMap[i][0]});
+            queue.offer(new int[] {i, n - 1, heightMap[i][n - 1]});
             visited[i][0] = visited[i][n - 1] = true;
         }
-
         for (int i = 0; i < n; i++) {
-            priorityQueue.offer(new int[] {0, i, heightMap[0][i]});
-            priorityQueue.offer(new int[] {m - 1, i, heightMap[m - 1][i]});
-
+            queue.offer(new int[] {0, i, heightMap[0][i]});
+            queue.offer(new int[] {m - 1, i, heightMap[m - 1][i]});
             visited[0][i] = visited[m - 1][i] = true;
         }
 
         int result = 0;
 
-        while (!priorityQueue.isEmpty()) {
-            int[] curr = priorityQueue.poll();
-            int row = curr[0];
-            int col = curr[1];
-            int height = curr[2];
+        while (!queue.isEmpty()) {
+            int[] a = queue.poll();
+            int r = a[0], c = a[1], h = a[2];
 
             for (int[] d : dir) {
-                int nextRow = row + d[0];
-                int nextCol = col + d[1];
+                int nr = r + d[0], nc = c + d[1];
 
-                if (isOutOfBoundary(nextRow, nextCol, heightMap) || visited[nextRow][nextCol]) {
+                if (isOutOfBoundary(nr, nc, heightMap) || visited[nr][nc]) {
                     continue;
                 }
 
-                int nextHeight = heightMap[nextRow][nextCol];
+                int nh = heightMap[nr][nc];
 
-                if (nextHeight < height) {
-                    result += height - heightMap[nextRow][nextCol];
-                    nextHeight = height;
+                if (nh < h) {
+                    result += h - nh;
+                    nh = h;
                 }
 
-                priorityQueue.offer(new int[] {nextRow, nextCol, nextHeight});
-                visited[nextRow][nextCol] = true;
+                queue.offer(new int[] {nr, nc, nh});
+                visited[nr][nc] = true;
             }
         }
 
         return result;
     }
 
-    private boolean isOutOfBoundary(int row, int col, int[][] grid) {
-        return row < 0 || col < 0 || row == grid.length || col == grid[0].length;
+    boolean isOutOfBoundary(int r, int c, int[][] grid) {
+        return r < 0 || c < 0 || r == grid.length || c == grid[0].length;
     }
 }
