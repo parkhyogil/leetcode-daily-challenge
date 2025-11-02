16b1d131bbc92966b2119a4ef5206452f1249b8a
hyogil <hg19910@gmail.com>
2025-11-02 13:49:40 +0900

    solve 2257-count-unguarded-cells-in-the-grid

--- problems/2257-count-unguarded-cells-in-the-grid/Solution.java
+++ problems/2257-count-unguarded-cells-in-the-grid/Solution.java
@@ -2,30 +2,26 @@ class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
         int[][] grid = new int[m][n];
 
-        for (int[] wall : walls) {
-            grid[wall[0]][wall[1]] = -1;
+        for (int[] g : guards) {
+            grid[g[0]][g[1]] = -1;
         }
 
-        for (int[] guard : guards) {
-            grid[guard[0]][guard[1]] = -1;
+        for (int[] w : walls) {
+            grid[w[0]][w[1]] = -1;
         }
-        
-        for (int[] guard : guards) {
-            int r = guard[0];
-            int c = guard[1];
+
+        for (int[] g : guards) {
+            int r = g[0], c = g[1];
 
             for (int i = r - 1; i >= 0 && grid[i][c] != -1; i--) {
                 grid[i][c] = 1;
             }
-
             for (int i = r + 1; i < m && grid[i][c] != -1; i++) {
                 grid[i][c] = 1;
             }
-
             for (int i = c - 1; i >= 0 && grid[r][i] != -1; i--) {
                 grid[r][i] = 1;
             }
-
             for (int i = c + 1; i < n && grid[r][i] != -1; i++) {
                 grid[r][i] = 1;
             }
