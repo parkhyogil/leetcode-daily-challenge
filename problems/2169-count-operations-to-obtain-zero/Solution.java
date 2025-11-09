4de5553b089630f30e384e1ec2bde3ba5a8265d3
hyogil <hg19910@gmail.com>
2025-11-09 11:29:03 +0900

    solve 2169-count-operations-to-obtain-zero

---
+++ problems/2169-count-operations-to-obtain-zero/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        if (num1 == 0 || num2 == 0) {
+            return 0;
+        }
+
+        int i;
+        if (num1 >= num2) {
+            i = num1 / num2;
+            num1 -= num2 * i;
+        } else {
+            i = num2 / num1;
+            num2 -= num1 * i;
+        }
+
+        return i + countOperations(num1, num2);
+    }
+}
