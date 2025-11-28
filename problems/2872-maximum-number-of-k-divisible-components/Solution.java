9efef77aace7391ba835c2249f68d594f5992a1e
hyogil <hg19910@gmail.com>
2025-11-28 13:58:45 +0900

    solve 2872-maximum-number-of-k-divisible-components

--- problems/2872-maximum-number-of-k-divisible-components/Solution.java
+++ problems/2872-maximum-number-of-k-divisible-components/Solution.java
@@ -5,23 +5,23 @@ class Solution {
         List<List<Integer>> graph = buildGraph(n, edges);
 
         result = 0;
-        
-        postorder(0, -1, graph, values, k);
+        recur(-1, 0, k, values, graph);
 
         return result;
     }
 
-    long postorder(int node, int parent, List<List<Integer>> graph, int[] values, int k) {
-        long sum = values[node];
+    int recur(int parent, int node, int k, int[] values, List<List<Integer>> graph) {
+        int sum = values[node] % k;
 
         for (int child : graph.get(node)) {
             if (child == parent) {
                 continue;
             }
-            sum += postorder(child, node, graph, values, k);
+
+            sum = (sum + recur(node, child, k, values, graph)) % k;
         }
 
-        if (sum % k == 0) {
+        if (sum == 0) {
             result++;
             return 0;
         }
@@ -31,17 +31,13 @@ class Solution {
 
     List<List<Integer>> buildGraph(int n, int[][] edges) {
         List<List<Integer>> graph = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+        for (int[] e : edges) {
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
         }
 
         return graph;
