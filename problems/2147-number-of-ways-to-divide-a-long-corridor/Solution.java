6b26f9db1ff9f417095329f06f401abca3d3cf2d
hyogil <hg19910@gmail.com>
2025-12-14 23:26:53 +0900

    solve 2147-number-of-ways-to-divide-a-long-corridor

--- problems/2147-number-of-ways-to-divide-a-long-corridor/Solution.java
+++ problems/2147-number-of-ways-to-divide-a-long-corridor/Solution.java
@@ -1,24 +1,50 @@
 class Solution {
-    private final int MOD = (int) 1e9 + 7;
-
     public int numberOfWays(String corridor) {
-        int seats = 0;
-        int plants = 0;
+        char[] arr = corridor.toCharArray();
+        int n = arr.length - 1;
 
-        long res = 1;
+        int i = 0, seats = 0, plants = 0;
 
-        for (char c : corridor.toCharArray()) {
-            if (c == 'S') {
-                if (seats % 2 == 0) {
-                    res = res * (plants + 1) % MOD;
-                    plants = 0;
-                }
+        while (i <= n && seats < 2) {
+            if(arr[i++] == 'S') {
                 seats++;
-            } else if (seats > 0 && seats % 2 == 0){
-                plants++;
             }
         }
 
-        return seats == 0 || seats % 2 == 1 ? 0 : (int) (res % MOD);
+        if (seats < 2) {
+            return 0;
+        }
+
+        seats = 0;
+
+        while (n >= 0 && seats < 2) {
+            if (arr[n--] == 'S') {
+                seats++;
+            }
+        }
+
+        if (n < i) {
+            return 1;
+        }
+
+        long result = 1;
+
+        while (i <= n) {
+            while (i <= n && arr[i] == 'P') {
+                i++;
+                plants++;
+            }
+
+            result = result * (plants + 1) % 1_000_000_007;
+            seats = plants = 0;
+
+            while (i <= n && seats < 2) {
+                if(arr[i++] == 'S') {
+                    seats++;
+                }
+            }
+        }
+
+        return seats % 2 != 0 ? 0 : (int) result;
     }
 }
