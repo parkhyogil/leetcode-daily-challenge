5e45ea388dad6929be2cd61891ac88e43cdd2ba3
hyogil <hg19910@gmail.com>
2025-12-19 22:25:35 +0900

    solve 2092-find-all-people-with-secret

--- problems/2092-find-all-people-with-secret/Solution.java
+++ problems/2092-find-all-people-with-secret/Solution.java
@@ -1,43 +1,58 @@
 class Solution {
     public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
-        List<List<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
+        List<List<int[]>> graph = buildGraph(n, meetings);
 
-        graph.get(0).add(new int[] {firstPerson, 0});
+        List<Integer> result = new ArrayList<>();
 
-        for (int[] meeting : meetings) {
-            graph.get(meeting[0]).add(new int[] {meeting[1], meeting[2]});
-            graph.get(meeting[1]).add(new int[] {meeting[0], meeting[2]});
-        }
+        int[] minTime = new int[n];
+        Arrays.fill(minTime, Integer.MAX_VALUE);
 
-        int[] meetTime = new int[n];
-        Arrays.fill(meetTime, Integer.MAX_VALUE);
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[1] - b[1]);
 
-        dfs(0, 0, graph, meetTime);
+        queue.offer(new int[] {0, 0});
+        queue.offer(new int[] {firstPerson, 0});
+        minTime[0] = minTime[firstPerson] = 0;
 
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (meetTime[i] != Integer.MAX_VALUE) {
-                res.add(i);
-            }
-        }
-        return new ArrayList<>(res);
-    }
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
 
-    private void dfs(int node, int time, List<List<int[]>> graph, int[] meetTime) {
-        meetTime[node] = time;
+            int i = curr[0], t = curr[1];
 
-        for (int[] next : graph.get(node)) {
-            int nextNode = next[0];
-            int nextTime = next[1];
-
-            if (nextTime < time || nextTime >= meetTime[nextNode]) {
+            if (t > minTime[i]) {
                 continue;
             }
 
-            dfs(nextNode, nextTime, graph, meetTime);
+            result.add(i);
+
+            for (int[] e : graph.get(i)) {
+                if (e[1] < t) {
+                    continue;
+                }
+
+                if (e[1] < minTime[e[0]]) {
+                    minTime[e[0]] = e[1];
+                    queue.offer(e);
+                }
+            }
         }
+
+        return result;
+    }
+
+    List<List<int[]>> buildGraph(int n, int[][] edges) {
+        List<List<int[]>> result = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            result.add(new ArrayList<>());
+        }
+
+        for (int[] e : edges) {
+            int u = e[0], v = e[1], t = e[2];
+
+            result.get(u).add(new int[] {v, t});
+            result.get(v).add(new int[] {u, t});
+        }
+
+        return result;
     }
 }
