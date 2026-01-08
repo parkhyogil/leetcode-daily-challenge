ca73f8985dc892b4173c58972d601e25c99c501a
hyogil <hg19910@gmail.com>
2026-01-08 23:15:28 +0900

    solve 1458-max-dot-product-of-two-subsequences

--- problems/1458-max-dot-product-of-two-subsequences/Solution.java
+++ problems/1458-max-dot-product-of-two-subsequences/Solution.java
@@ -1,46 +1,22 @@
 class Solution {
-    private int[][] cache;
-
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        int min1 = Integer.MAX_VALUE;
-        int max1 = Integer.MIN_VALUE;
-        int min2 = Integer.MAX_VALUE;
-        int max2 = Integer.MIN_VALUE;
+        int n = nums1.length;
+        int m = nums2.length;
 
-        for (int num : nums1) {
-            min1 = Math.min(min1, num);
-            max1 = Math.max(max1, num);
-        }
-
-        for (int num : nums2) {
-            min2 = Math.min(min2, num);
-            max2 = Math.max(max2, num);
-        }
-
-        if (min1 > 0 && max2 < 0) {
-            return min1 * max2;
-        }
-
-        if (max1 < 0 && min2 > 0) {
-            return max1 * min2;
-        }
-
-        cache = new int[nums1.length][nums2.length];
-
-        return recur(0, 0, nums1, nums2);
+        return recur(n - 1, m - 1, nums1, nums2, new Integer[n][m]);
     }
 
-    private int recur(int i, int j, int[] nums1, int[] nums2) {
-        if (i == nums1.length || j == nums2.length) {
-            return 0;
+    int recur(int i, int j, int[] nums1, int[] nums2, Integer[][] cache) {
+        if (i < 0 || j < 0) {
+            return Integer.MIN_VALUE / 2;
         }
 
-        if (cache[i][j] != 0) {
+        if (cache[i][j] != null) {
             return cache[i][j];
         }
 
-        int res = nums1[i] * nums2[j] + recur(i + 1, j + 1, nums1, nums2);
-        res = Math.max(res, Math.max(recur(i + 1, j, nums1, nums2), recur(i, j + 1, nums1, nums2)));
-        return cache[i][j] = res;
+        int take = nums1[i] * nums2[j] + Math.max(recur(i - 1, j - 1, nums1, nums2, cache), 0);
+        
+        return cache[i][j] = Math.max(take, Math.max(recur(i - 1, j, nums1, nums2, cache), recur(i, j - 1, nums1, nums2, cache)));
     }
 }
