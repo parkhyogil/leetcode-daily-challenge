534a04b1bb697aaee6323df347ede0417fff0dba
hyogil <hg19910@gmail.com>
2026-01-10 20:27:09 +0900

    solve 0712-minimum-ascii-delete-sum-for-two-strings

--- problems/0712-minimum-ASCII-delete-sum-for-two-strings/Solution.java
+++ problems/0712-minimum-ASCII-delete-sum-for-two-strings/Solution.java
@@ -1,27 +1,31 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
+        int n = s1.length();
+        int m = s2.length();
 
-        int[][] dp = new int[m + 1][n + 1];
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 0; i < n; i++) {
+            dp[i + 1][0] = dp[i][0] + (int) s1.charAt(i);
+        }
+
         for (int i = 0; i < m; i++) {
-            dp[i + 1][0] = dp[i][0] + s1.charAt(i);
+            dp[0][i + 1] = dp[0][i] + (int) s2.charAt(i);
         }
 
         for (int i = 0; i < n; i++) {
-            dp[0][i + 1] = dp[0][i] + s2.charAt(i);
-        }
+            for (int j = 0; j < m; j++) {
+                int a = (int) s1.charAt(i);
+                int b = (int) s2.charAt(j);
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (s1.charAt(i) == s2.charAt(j)) {
+                if (a == b) {
                     dp[i + 1][j + 1] = dp[i][j];
                 } else {
-                    dp[i + 1][j + 1] = Math.min(dp[i + 1][j] + s2.charAt(j), dp[i][j + 1] + s1.charAt(i));
+                    dp[i + 1][j + 1] = Math.min(a + dp[i][j + 1], b + dp[i + 1][j]);
                 }
             }
         }
 
-        return dp[m][n];
+        return dp[n][m];
     }
 }
