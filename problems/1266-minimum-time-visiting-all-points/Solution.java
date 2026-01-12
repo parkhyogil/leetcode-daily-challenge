1a02d7e013ca46fe2a857bd913f539ae247df75b
hyogil <hg19910@gmail.com>
2026-01-12 17:28:44 +0900

    solve 1266-minimum-time-visiting-all-points

--- problems/1266-minimum-time-visiting-all-points/Solution.java
+++ problems/1266-minimum-time-visiting-all-points/Solution.java
@@ -2,17 +2,12 @@ class Solution {
     public int minTimeToVisitAllPoints(int[][] points) {
         int n = points.length;
 
-        int res = 0;
+        int result = 0;
+
         for (int i = 0; i < n - 1; i++) {
-            res += getTime(points[i], points[i + 1]);
+            result += Math.max(Math.abs(points[i][0] - points[i + 1][0]), Math.abs(points[i][1] - points[i + 1][1]));
         }
-        return res;
-    }
 
-    private int getTime(int[] p1, int[] p2) {
-        int w = Math.abs(p1[0] - p2[0]);
-        int h = Math.abs(p1[1] - p2[1]);
-
-        return Math.max(w, h);        
+        return result;
     }
 }
