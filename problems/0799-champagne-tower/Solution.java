af2c4a3ca98f038980728871344f1ae92f109627
hyogil <hg19910@gmail.com>
2026-02-14 22:59:46 +0900

    solve 0799-champagne-tower

--- problems/0799-champagne-tower/Solution.java
+++ problems/0799-champagne-tower/Solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        double[] dp = new double[query_row + 1];
+        double[] dp = new double[query_glass + 2];
 
         dp[0] = poured;
 
         for (int i = 0; i < query_row; i++) {
-            for (int j = i; j >= 0; j--) {
-                double spill = (dp[j] - 1) / 2;
+            for (int j = Math.min(i, query_glass); j >= 0; j--) {
+                double spill = dp[j] - 1;
 
                 if (spill > 0) {
-                    dp[j] = spill;
-                    dp[j + 1] += spill;
+                    dp[j + 1] += spill / 2;
+                    dp[j] = spill / 2;
                 } else {
                     dp[j] = 0;
                 }
