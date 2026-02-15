f4b80ca80818f26a110bf8afebd8dd3306c0cb3d
hyogil <hg19910@gmail.com>
2026-02-15 21:27:56 +0900

    solve 0067-add-binary

--- problems/0067-add-binary/Solution.java
+++ problems/0067-add-binary/Solution.java
@@ -1,27 +1,22 @@
 class Solution {
     public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+
         int i = a.length() - 1;
         int j = b.length() - 1;
 
         int carry = 0;
-        StringBuilder sb = new StringBuilder();
-        while (i >= 0 || j >= 0) {
-            int sum = carry;
-            if (i >= 0 && a.charAt(i) == '1') {
-                sum++;
-            }
-            if (j >= 0 && b.charAt(j) == '1') {
-                sum++;
-            }
+        
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = (i < 0 ? 0 : a.charAt(i) - '0') + (j < 0 ? 0 : b.charAt(j) - '0') + carry;
 
             sb.append(sum % 2);
             carry = sum / 2;
+
             i--;
             j--;
         }
-        if (carry == 1) {
-            sb.append(1);
-        }
+
         return sb.reverse().toString();
     }
 }
