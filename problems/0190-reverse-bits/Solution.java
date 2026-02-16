d28035428e176f09aea6f9999fe94e4ffa2c5a29
hyogil <hg19910@gmail.com>
2026-02-16 13:38:04 +0900

    solve 0190-reverse-bits

---
+++ problems/0190-reverse-bits/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result = result * 2 + n % 2;
+            n /= 2;
+        }
+
+        return result;
+    }
+}
