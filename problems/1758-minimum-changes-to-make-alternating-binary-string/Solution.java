66c1425f012c9d4b823320127edd441e04a3d323
hyogil <hg19910@gmail.com>
2026-03-05 13:21:02 +0900

    solve 1758-minimum-changes-to-make-alternating-binary-string

--- problems/1758-minimum-changes-to-make-alternating-binary-string/Solution.java
+++ problems/1758-minimum-changes-to-make-alternating-binary-string/Solution.java
@@ -2,15 +2,16 @@ class Solution {
     public int minOperations(String s) {
         int n = s.length();
 
-        int res = 0;
+        int result = 0;
+
         for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            
-            if ((i % 2 == 0 && c == '1') || i % 2 == 1 && c == '0') {
-                res++;
+            int c = s.charAt(i) - '0';
+
+            if ((i % 2 == 0 && c == 0) || (i % 2 == 1 && c == 1)) {
+                result++;
             }
         }
 
-        return Math.min(res, n - res);
+        return Math.min(result, n - result);
     }
 }
