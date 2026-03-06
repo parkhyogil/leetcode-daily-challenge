561abe5db33b235be32f0eb5293e63ffc3478ffd
hyogil <hg19910@gmail.com>
2026-03-06 09:47:55 +0900

    solve 1784-check-if-binary-string-has-at-most-one-segment-of-ones

---
+++ problems/1784-check-if-binary-string-has-at-most-one-segment-of-ones/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        int n = s.length();
+
+        int i = 0;
+
+        while (i < n && s.charAt(i) == '1') {
+            i++;
+        }
+        while (i < n && s.charAt(i) == '0') {
+            i++;
+        }
+
+        return i == n;
+    }
+}
