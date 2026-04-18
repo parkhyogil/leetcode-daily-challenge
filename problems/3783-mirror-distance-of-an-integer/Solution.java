5c2307bf82797df90bf253f5eb497fe1dd41d098
hyogil <hg19910@gmail.com>
2026-04-18 15:46:43 +0900

    solve 3783-mirror-distance-of-an-integer

---
+++ problems/3783-mirror-distance-of-an-integer/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int mirrorDistance(int n) {
+        return Math.abs(n - reverse(n));
+    }
+
+    int reverse(int n) {
+        int result = 0;
+
+        while (n > 0) {
+            result = result * 10 + n % 10;
+            n /= 10;
+        }
+
+        return result;
+    }
+}
