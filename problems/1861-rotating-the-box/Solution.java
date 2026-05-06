47c63176f10fa7894108fffda97788c39640fc0b
hyogil <hg19910@gmail.com>
2026-05-06 14:17:44 +0900

    solve 1861-rotating-the-box

--- problems/1861-rotating-the-box/Solution.java
+++ problems/1861-rotating-the-box/Solution.java
@@ -1,42 +1,25 @@
 class Solution {
-    public char[][] rotateTheBox(char[][] box) {
-        int m = box.length;
-        int n = box[0].length;
-
-        for (char[] row : box) {
-            pushToRight(row);
-        }
-
-        return rotate(box);
-    }
-
-    char[][] rotate(char[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
 
         char[][] result = new char[n][m];
 
         for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                result[c][m - 1 - r] = matrix[r][c];
+            int i = n;
+            for (int c = n - 1; c >= 0; c--) {
+                if (boxGrid[r][c] == '*') {
+                    result[c][m - 1 - r] = '*';
+                    i = c;
+                } else if (boxGrid[r][c] == '#') {
+                    result[c][m - 1 - r] = '.';
+                    result[--i][m - 1 - r] = '#';
+                } else {
+                    result[c][m - 1 - r] = '.';
+                }
             }
         }
 
         return result;
-    }
-
-    void pushToRight(char[] row) {
-        int length = row.length;
-
-        int idx = length;
-
-        for (int i = length - 1; i >= 0; i--) {
-            if (row[i] == '#') {
-                row[i] = '.';
-                row[--idx] = '#';
-            } else if (row[i] == '*') {
-                idx = i;
-            }
-        }
     }
 }
