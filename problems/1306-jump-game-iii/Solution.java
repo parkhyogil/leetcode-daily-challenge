215fa433bf6306ebe6b8543dd9f955a43ea1cf56
hyogil <hg19910@gmail.com>
2026-05-17 23:26:33 +0900

    solve 1306-jump-game-iii

---
+++ problems/1306-jump-game-iii/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        if (start < 0 || start >= arr.length || arr[start] == -1) {
+            return false;
+        }
+
+        if (arr[start] == 0) {
+            return true;
+        }
+
+        int x = arr[start];
+        arr[start] = -1;
+
+        return canReach(arr, start - x) || canReach(arr, start + x);
+    }
+}
