068d2aeaed970c684f4f2a37898606d2d6b22282
hyogil <hg19910@gmail.com>
2026-05-23 23:23:42 +0900

    solve 1752-check-if-array-is-sorted-and-rotated

--- problems/1752-checki-if-array-is-sorted-and-rotated/Solution.java
+++ problems/1752-checki-if-array-is-sorted-and-rotated/Solution.java
@@ -2,18 +2,20 @@ class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
 
-        int begin = 1;
+        int minIdx = -1;
 
-        while (begin < n && nums[begin - 1] <= nums[(begin) % n]) {
-            begin++;
+        for (int i = 0; i < n; i++) {
+            if (minIdx == -1 || nums[i] < nums[minIdx]) {
+                minIdx = i;
+            }
         }
 
-        if (begin == n) {
-            return true;
+        for (int i = 0; i < n && nums[minIdx] == nums[(minIdx - 1 + n) % n]; i++) {
+            minIdx = (minIdx - 1 + n) % n;
         }
 
         for (int i = 0; i < n - 1; i++) {
-            if (nums[(begin + i) % n] > nums[(begin + i + 1) % n]) {
+            if (nums[(minIdx + i) % n] > nums[(minIdx + i + 1) % n]) {
                 return false;
             }
         }
