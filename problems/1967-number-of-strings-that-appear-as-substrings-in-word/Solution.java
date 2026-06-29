9ab615fc9c329ba5c07435d9e4625e265b88318d
hyogil <hg19910@gmail.com>
2026-06-29 23:22:46 +0900

    solve 1967-number-of-strings-that-appear-as-substrings-in-word

---
+++ problems/1967-number-of-strings-that-appear-as-substrings-in-word/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int result = 0;
+
+        for (String s : patterns) {
+            if (word.contains(s)) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
