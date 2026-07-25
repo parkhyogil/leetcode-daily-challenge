8e839b2af9c8df762292e0e8b4ac28e1ff7812b1
hyogil <hg19910@gmail.com>
2026-07-25 20:49:40 +0900

    solve 3536-maximum-product-of-two-digits

---
+++ problems/3536-maximum-product-of-two-digits/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int n) {
+        int a = 0;
+        int b = 0;
+
+        while (n > 0) {
+            int d = n % 10;
+            n /= 10;
+
+            if (d > a) {
+                b = a;
+                a = d;
+            } else if (d > b) {
+                b = d;
+            }
+        }
+
+        return a * b;
+    }
+}
