365ed2cbad43d6c730b5426652062f1305cdb364
hyogil <hg19910@gmail.com>
2026-07-31 11:40:24 +0900

    solve 3016-minimum-number-of-pushes-to-type-word-ii

--- problems/3016-minimum-number-of-pushes-to-type-word-ii/Solution.java
+++ problems/3016-minimum-number-of-pushes-to-type-word-ii/Solution.java
@@ -1,26 +1,17 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] numberOfLetters = new int[26];
+        int[] freq = new int[26];
 
         for (char c : word.toCharArray()) {
-            numberOfLetters[c - 'a']++;
+            freq[c - 'a']++;
         }
 
-        Arrays.sort(numberOfLetters);
-
-        int push = 1;
-        int unmappedButtons = 8;
+        Arrays.sort(freq);
 
         int result = 0;
 
-        for (int i = 25; i >= 0 && numberOfLetters[i] > 0; i--) {
-            if (unmappedButtons == 0) {
-                push++;
-                unmappedButtons = 8;
-            }
-
-            result += push * numberOfLetters[i];
-            unmappedButtons--;
+        for (int i = 0; i < 26 && freq[25 - i] > 0; i++) {
+            result += freq[25 - i] * (i / 8 + 1);
         }
 
         return result;
