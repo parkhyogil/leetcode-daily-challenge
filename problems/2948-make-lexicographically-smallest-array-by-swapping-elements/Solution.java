7f575e3d1317aff079c38868f0115bf0707e03ff
hyogil <hg19910@gmail.com>
2026-08-29 20:43:11 +0900

    solve 2948-make-lexicographically-smallest-array-by-swapping-elements

--- problems/2948-make-lexicographically-smallest-array-by-swapping-elements/Solution.java
+++ problems/2948-make-lexicographically-smallest-array-by-swapping-elements/Solution.java
@@ -2,29 +2,31 @@ class Solution {
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
         int n = nums.length;
 
-        List<Integer> indexList = new ArrayList<>();
+        List<Integer> idx = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            indexList.add(i);
+            idx.add(i);
         }
 
-        indexList.sort((a, b) -> Integer.compare(nums[a], nums[b]));
+        idx.sort((a, b) -> nums[a] - nums[b]);
 
-        PriorityQueue<Integer> highestIndex = new PriorityQueue<>(Collections.reverseOrder());
-        highestIndex.offer(indexList.get(0));
+        int[] id = new int[n];
+        int[] start = new int[n];
+
+        for (int i = 1; i < n; i++) {
+            int a = idx.get(i);
+            int b = idx.get(i - 1);
+
+            id[a] = id[b];
+            if (nums[a] - nums[b] > limit) {
+                id[a]++;
+                start[id[a]] = i;
+            }
+        }
 
         int[] result = new int[n];
 
-        for (int i = 1; i < n; i++) {
-            if (nums[indexList.get(i)] - nums[indexList.get(i - 1)] > limit) {
-                for (int j = i - 1; !highestIndex.isEmpty(); j--) {
-                    result[highestIndex.poll()] = nums[indexList.get(j)];
-                }
-            }
-            highestIndex.offer(indexList.get(i));
-        }
-
-        for (int j = n - 1; !highestIndex.isEmpty(); j--) {
-            result[highestIndex.poll()] = nums[indexList.get(j)];
+        for (int i = 0; i < n; i++) {
+            result[i] = nums[idx.get(start[id[i]]++)];
         }
 
         return result;
